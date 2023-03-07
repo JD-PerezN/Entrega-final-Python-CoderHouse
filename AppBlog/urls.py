ff2c7cd1a35c317d10afd55bde98a52ad4411f08
JD_PerezN <jd.perez998@gmail.com>
2023-03-07 14:17:59 -0500

    Update: First attempt blog template

---
+++ AppBlog/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from django.contrib.auth.views import LogoutView
+
+from AppBlog import views
+
+urlpatterns = [
+    #path("", views.inicio, name="inicio"),
+    #path("cursos/", views.cursos, name="cursos"),
+    #path("profesores/", views.profesores, name="profesores"),
+    #path("estudiantes/", views.estudiantes, name="estudiantes"),
+    #path("entregables/", views.entregables, name="entregables"),
+    ##path("curso-formulario/", views.curso_formulario, name="curso-formulario"),
+    ##path("profesor-formulario/", views.profesor_formulario, name="profesor-formulario"),
+    ##path("busqueda-camada/", views.busqueda_camada, name="busqueda-camada"),
+    #path("buscar/", views.buscar, name="buscar"),
+    #path("leer-profesores/", views.leer_profesores, name="leer-profesores"),
+    #path("eliminar-profesor/<profesor_id>", views.eliminar_profesor, name="eliminar-profesor"),
+    #path("editar-profesor/<profesor_id>", views.editar_profesor, name="editar-profesor")
+    path("", views.PostList.as_view(), name="appblog-inicio"),
+    path("detalle/<slug:slug>", views.PostDetail.as_view(), name="appblog-detalle")
+]
--- AppBlog/views.py
+++ AppBlog/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from AppBlog import models
+
+## Create your views here.
+# VIEW TO SEE THE LIST OF POST
+class PostList(ListView):
+
+    queryset = models.Post.objects.filter(status=1).order_by("-created_on")
+    template_name = "AppBlog/post-list.html"
+
+# VIEW TO CHECK THE DETAIL OF A PARTICULAR POST
+class PostDetail(DetailView):
+
+    model = models.Post
+    template_name = "AppBlog/post-detail.html"
